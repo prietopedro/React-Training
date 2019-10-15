f1a5ac01d4ab3bce56b5bdce2d8136bea9a2e20b
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-14 22:07:21 -0400

    Set Active ClassName for NavLinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Link} from "react-router-dom"
+import {NavLink, Link} from "react-router-dom"
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`} ><span className="saved-movie">{movie.title}</span></Link>
+      <NavLink activeClassName="active" to={`/movies/${movie.id}`} ><span className="saved-movie">{movie.title}</span></NavLink>
     ))}
     <Link to="/"><div className="home-button">Home</div></Link>
   </div>
