3026e447f726c50f6df0fabe46d80650052b3b3b
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-30 16:54:32 -0400

    conditional link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import useDarkMode from "./hooks/useDarkMode"
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
   const [darkMode, setDarkMode, changeColors] = useDarkMode(false);
@@ -13,7 +14,7 @@ const Navbar = () => {
   console.log(darkMode)
   return (
     <nav className={`navbar ${darkMode ? 'dark-mode' : ""}`}>
-      <h1>Crypto Tracker</h1>
+      <Link id={darkMode ? 'dark-mode-links' : 'Links'} to="/"><h1>Crypto Tracker</h1></Link>
       <div className="dark-mode__toggle" onClick={changeColors}>
         <div
           className={darkMode ? 'toggle toggled' : 'toggle'}
