1c1b21ea2b6b016f8dcc664108ee08e6ecd4a0f8
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-31 11:24:00 -0400

    body

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import useDarkMode from "./hooks/useDarkMode"
 import {Link} from "react-router-dom"
 
-const Navbar = () => {
-  const [darkMode, setDarkMode, changeColors] = useDarkMode(false);
+const Navbar = ({darkMode,changeColors}) => {
+  // const [darkMode, setDarkMode, changeColors] = useDarkMode(false);
   // const toggleMode = e => {
   //   e.preventDefault();
   //   setDarkMode(!darkMode);
@@ -13,7 +13,7 @@ const Navbar = () => {
   // },[darkMode])
   console.log(darkMode)
   return (
-    <nav className={`navbar ${darkMode ? 'dark-mode' : ""}`}>
+    <nav className="navbar">
       <Link id={darkMode ? 'dark-mode-links' : 'Links'} to="/"><h1>Crypto Tracker</h1></Link>
       <div className="dark-mode__toggle" onClick={changeColors}>
         <div
--- src/index.js
+++ src/index.js
@@ -6,24 +6,34 @@ import Coin from "./components/Coin.jsx"
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-
+import useDarkMode from "./components/hooks/useDarkMode"
 import "./styles.scss";
+
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [darkMode, setDarkMode, changeColors] = useDarkMode(false);
 
+  
   useEffect(() => {
     axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+    .get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
-  }, []);
-  console.log(coinData)
+    }, []);
+    console.log(coinData)
+    
+  useEffect(()=>{
+    let body = document.querySelector("body")
+    darkMode ? body.classList.add('dark-mode') : body.classList.remove("dark-mode")
+  },[darkMode])
+
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar darkMode={darkMode} changeColors={changeColors} />
       <Switch>
         <Route path="/" exact render={(props)=><Charts {...props} coinData={coinData} />} />
         <Route path="/:coin" exact render={(props)=> <Coin {...props}/>} />
