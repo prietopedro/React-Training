871d16e9cb67e4b7cea896ca2140bd20f2fd5206
Pedro Prieto <pedro.prieto003@gmail.com>
2019-11-08 13:13:09 -0500

    Refactored Reducer

--- smurfs/src/Actions/index.js
+++ smurfs/src/Actions/index.js
@@ -1,57 +1,47 @@
 import axios from "axios"
 
 
-export const SMURF_GET_START = "SMURF_GET_START"
-export const SMURF_GET_SUCCESS = "SMURF_GET_SUCCESS"
-export const SMURF_GET_FAIL = "SMURF_GET_FAIL"
-
-export const SMURF_POST_START = "SMURF_POST_START"
-export const SMURF_POST_SUCCESS = "SMURF_POST_SUCCESS"
-export const SMURF_POST_FAIL = "SMURF_POST_FAIL"
-
-export const SMURF_DELETE_START = "SMURF_DELETE_START"
-export const SMURF_DELETE_SUCCESS = "SMURF_DELETE_SUCCESS"
-export const SMURF_DELETE_FAIL = "SMURF_DELETE_FAIL"
-
-export const SMURF_PUT_START = "SMURF_PUT_START"
-export const SMURF_PUT_SUCCESS = "SMURF_PUT_SUCCESS"
-export const SMURF_PUT_FAIL = "SMURF_PUT_FAIL"
+export const SMURF_FETCH_START = "SMURF_GET_START"
+export const SMURF_FETCH_SUCCESS = "SMURF_GET_SUCCESS"
+export const SMURF_FETCH_FAIL = "SMURF_GET_FAIL"
 
 export const SET_EDITING = "SET_EDITING"
 
+// GET 
 export const smurfsGet = () => dispatch => {
-    dispatch({type:SMURF_GET_START})
+    dispatch({type:SMURF_FETCH_START})
     axios
     .get("http://localhost:3333/smurfs")
-    .then(res=>dispatch({type:SMURF_GET_SUCCESS, payload:res.data}))
-    .catch(err=>dispatch({type:SMURF_GET_FAIL, payload: err.message}))
+    .then(res=>dispatch({type:SMURF_FETCH_SUCCESS, payload:res.data}))
+    .catch(err=>dispatch({type:SMURF_FETCH_FAIL, payload: err.message}))
 }
-
+// POST
 export const smurfPost = smurf => dispatch => {
-    dispatch({type:SMURF_POST_START})
+    dispatch({type:SMURF_FETCH_START})
     axios
     .post("http://localhost:3333/smurfs",smurf)
-    .then(res=>dispatch({type:SMURF_POST_SUCCESS, payload:res.data}))
-    .catch(err=>dispatch({type:SMURF_POST_FAIL, payload: err.message}))
+    .then(res=>dispatch({type:SMURF_FETCH_SUCCESS, payload:res.data}))
+    .catch(err=>dispatch({type:SMURF_FETCH_FAIL, payload: err.message}))
 }
-
+// DELETE
 export const smurfDelete = (id) => dispatch => {
-    dispatch({type:SMURF_DELETE_START})
+    dispatch({type:SMURF_FETCH_START})
     axios
     .delete(`http://localhost:3333/smurfs/${id}`)
-    .then(res=>dispatch({type:SMURF_DELETE_SUCCESS, payload:res.data}))
-    .catch(err=>dispatch({type:SMURF_DELETE_FAIL, payload:err.message}))
+    .then(res=>dispatch({type:SMURF_FETCH_SUCCESS, payload:res.data}))
+    .catch(err=>dispatch({type:SMURF_FETCH_FAIL, payload:err.message}))
+}
+// PUT
+export const smurfEdit = smurf => dispatch => {
+    dispatch({type:SMURF_FETCH_START})
+    axios
+    .put(`http://localhost:3333/smurfs/${smurf.id}`,smurf)
+    .then(res=>dispatch({type:SMURF_FETCH_SUCCESS, payload:res.data}))
+    .catch(err=>dispatch({type:SMURF_FETCH_FAIL, payload:err.message}))
 }
 
 export const setEdit = smurf => {
     return ({type: SET_EDITING , payload: smurf})
 }
 
-export const smurfEdit = smurf => dispatch => {
-    dispatch({type:SMURF_PUT_START})
-    axios
-    .put(`http://localhost:3333/smurfs/${smurf.id}`,smurf)
-    .then(res=>dispatch({type:SMURF_PUT_SUCCESS, payload:res.data}))
-    .catch(err=>dispatch({type:SMURF_PUT_FAIL, payload:err.message}))
-}
 
--- smurfs/src/Reducers/index.js
+++ smurfs/src/Reducers/index.js
@@ -1,4 +1,4 @@
-import {SMURF_GET_START,SMURF_GET_FAIL,SMURF_GET_SUCCESS,SMURF_POST_START,SMURF_POST_SUCCESS,SMURF_POST_FAIL, SMURF_DELETE_FAIL,SMURF_DELETE_START,SMURF_DELETE_SUCCESS, SET_EDITING,SMURF_PUT_SUCCESS,SMURF_PUT_FAIL,SMURF_PUT_START} from "../Actions"
+import {SMURF_FETCH_START,SMURF_FETCH_FAIL,SMURF_FETCH_SUCCESS,SET_EDITING} from "../Actions"
 
 const initialState = {
     smurfs: [],
@@ -10,7 +10,7 @@ const initialState = {
 
 export const reducer = (state = initialState, {type , payload}) => {
     switch(type){
-        case SMURF_GET_START:
+        case SMURF_FETCH_START:
             return {
                 ...state,
                 smurfs: [],
@@ -19,7 +19,7 @@ export const reducer = (state = initialState, {type , payload}) => {
                 isEditing: false,
                 editingSmurf: {}
             };
-        case SMURF_GET_SUCCESS:
+        case SMURF_FETCH_SUCCESS:
             return {
                 ...state,
                 smurfs: payload,
@@ -28,61 +28,7 @@ export const reducer = (state = initialState, {type , payload}) => {
                 isEditing: false,
                 editingSmurf: {}
             };
-        case SMURF_GET_FAIL:
-            return {
-                ...state,
-                smurfs: [],
-                error: payload,
-                isFetching: false,
-                isEditing: false,
-                editingSmurf: {}
-            }
-        case SMURF_POST_START:
-            return {
-                ...state,
-                smurfs: [],
-                isFetching: true,
-                error: null,
-                isEditing: false,
-                editingSmurf: {}
-            };
-        case SMURF_POST_SUCCESS:
-            return {
-                ...state,
-                smurfs: payload,
-                error: null,
-                isFetching: false,
-                isEditing: false,
-                editingSmurf: {}
-            };
-        case SMURF_POST_FAIL:
-            return {
-                ...state,
-                smurfs: [],
-                error: payload,
-                isFetching: false,
-                isEditing: false,
-                editingSmurf: {}
-            }
-        case SMURF_DELETE_START:
-            return {
-                ...state,
-                smurfs: [],
-                isFetching: true,
-                error: null,
-                isEditing: false,
-                editingSmurf: {}
-            };
-        case SMURF_DELETE_SUCCESS:
-            return {
-                ...state,
-                smurfs: payload,
-                error: null,
-                isFetching: false,
-                isEditing: false,
-                editingSmurf: {}
-            };
-        case SMURF_DELETE_FAIL:
+        case SMURF_FETCH_FAIL:
             return {
                 ...state,
                 smurfs: [],
@@ -96,33 +42,6 @@ export const reducer = (state = initialState, {type , payload}) => {
                 ...state,
                 isEditing: true,
                 editingSmurf: payload
-            }
-        case SMURF_PUT_START:
-            return {
-                ...state,
-                smurfs: [],
-                isFetching: true,
-                error: null,
-                isEditing: false,
-                editingSmurf: {}
-            };
-        case SMURF_PUT_SUCCESS:
-            return {
-                ...state,
-                smurfs: payload,
-                error: null,
-                isFetching: false,
-                isEditing: false,
-                editingSmurf: {}
-            };
-        case SMURF_PUT_FAIL:
-            return {
-                ...state,
-                smurfs: [],
-                error: payload,
-                isFetching: false,
-                isEditing: false,
-                editingSmurf: {}
             }
         default:
             return state
