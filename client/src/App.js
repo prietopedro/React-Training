8e6f9f8e903f9c703aa260b5d41c3d21f3df107d
Pedro Prieto <pedro.prieto003@gmail.com>
2019-11-15 12:02:21 -0500

    Edit and Delete and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route , Redirect} from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute"
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage"
+
 import "./styles.scss";
 
 function App() {
@@ -13,6 +16,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/BubblePage" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState , useEffect } from "react";
+import axiosWithAuth from "../axiosWithAuth"
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(()=>{
+    axiosWithAuth()
+    .get("/api/colors")
+    .then(res=>setColorList(res.data))
+    .catch(err=>console.log(err))
+  },[])
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
